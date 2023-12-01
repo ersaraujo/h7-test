fffb919d2bf3865a6c6b6648d4cc8c3d7753bf56
Julia <jedl@cin.ufpe.br>
2023-11-30 23:15:13 -0300

    Fix: init timer 8

--- lib/Encoder/Encoder.cpp
+++ lib/Encoder/Encoder.cpp
@@ -9,28 +9,27 @@ Encoder::Encoder(int pulsesPerRev, int tsample, TIM_TypeDef *timer)
 
 void Encoder::init()
 {
-    init_enc_timer(this->_timer);
-    this->resetPulses();
-    _controlTicker.attach(callback(this, &Encoder::update), chrono::milliseconds(_tsample));
-
+  init_enc_timer(this->_timer);
+  this->resetPulses();
+  _controlTicker.attach(callback(this, &Encoder::update), chrono::milliseconds(_tsample));
 }
 
 void Encoder::update()
 {
-    
-    _frequency = double((this->getPulses() * (1.0 / (_tsample/1000.0))) / (4.0 * _pulsesPerRev));
-    this->resetPulses();
-    // printf("Frequency: %lf | Pulses: %d \n", _frequency, _pulses);
+
+  _frequency = double((this->getPulses() * (1.0 / (_tsample / 1000.0))) / (4.0 * _pulsesPerRev));
+  this->resetPulses();
+  // printf("Frequency: %lf | Pulses: %d \n", _frequency, _pulses);
 }
 
 double Encoder::getFrequency()
 {
-    return _frequency;
+  return _frequency;
 }
 
 double Encoder::getSpeed()
 {
-    return 2.0 * M_PI * (MOTOR_GEAR/WHEEL_GEAR) * this->getFrequency();
+  return 2.0 * M_PI * (MOTOR_GEAR / WHEEL_GEAR) * this->getFrequency();
 }
 
 void Encoder::resetPulses()
@@ -42,52 +41,43 @@ void Encoder::resetPulses()
 
 int Encoder::getPulses()
 {
-    _pulses = PULSES_RESET - this->_timer->CNT;
-    return _pulses;
+  _pulses = PULSES_RESET - this->_timer->CNT;
+  return _pulses;
 }
 
 void Encoder::init_enc_timer(TIM_TypeDef *timer)
 {
-  if (timer == TIM1){
+  if (timer == TIM1)
+  {
     init_enc_timer1();
-    init_enc_timer2();
-    init_enc_timer3();
+  }
+  else if (timer == TIM8)
+  {
     init_enc_timer8();
   }
-  else if (timer == TIM8){
-    init_enc_timer1();
+  else if (timer == TIM2)
+  {
     init_enc_timer2();
-    init_enc_timer3();
-    init_enc_timer8();
   }
-  else if (timer == TIM2){
-    init_enc_timer1();
-    init_enc_timer2();
+  else if (timer == TIM3)
+  {
     init_enc_timer3();
-    init_enc_timer8();
-  }
-  else if (timer == TIM3){
-    init_enc_timer1();
-    init_enc_timer2();
-    init_enc_timer3();
-    init_enc_timer8();
   }
 }
 
-
 // AFR[0] geralmente é associado aos pinos de 0 a 7, enquanto AFR[1] é associado aos pinos de 8 a 15.
 
-void Encoder::init_enc_timer1()
+void Encoder::init_enc_timer1() // PE_9 PA_9 (PG_12)
 {
-// Habilitar o clock para o Timer 1 e os portos GPIOE e GPIOA
+  // Habilitar o clock para o Timer 1 e os portos GPIOE e GPIOA
   RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
   RCC->AHB4ENR |= RCC_AHB4ENR_GPIOEEN | RCC_AHB4ENR_GPIOAEN;
 
   // Configurar pinos GPIO para modo alternativo
-  GPIOE->MODER &= ~GPIO_MODER_MODE9; // Limpar bits
+  GPIOE->MODER &= ~GPIO_MODER_MODE9;  // Limpar bits
   GPIOE->MODER |= GPIO_MODER_MODE9_1; // Modo alternativo
 
-  GPIOA->MODER &= ~GPIO_MODER_MODE9; // Limpar bits
+  GPIOA->MODER &= ~GPIO_MODER_MODE9;  // Limpar bits
   GPIOA->MODER |= GPIO_MODER_MODE9_1; // Modo alternativo
 
   GPIOE->OTYPER |= GPIO_OTYPER_OT9; // Saída push-pull
@@ -112,61 +102,64 @@ void Encoder::init_enc_timer1()
   TIM1->DIER = 0;
 
   // Configurar modo de encoder
-  TIM1->SMCR = 3; // Modo de encoder 3 (conta todas as transições)
+  TIM1->SMCR = 3;          // Modo de encoder 3 (conta todas as transições)
   TIM1->CR1 = TIM_CR1_CEN; // Habilitar o contador
 
-  printf("TIM1->CR1: %X\n", TIM1->CR1);      // Configurações de controle do timer
-  printf("TIM1->SMCR: %X\n", TIM1->SMCR);    // Configurações de controle do modo do timer
-  printf("TIM1->CCMR1: %X\n", TIM1->CCMR1);  // Configurações de captura/comparação do canal 1
-  printf("TIM1->CCMR2: %X\n", TIM1->CCMR2);  // Configurações de captura/comparação do canal 2
-  printf("TIM1->CCER: %X\n", TIM1->CCER);    // Configurações de controle dos canais de captura/comparação
-  printf("TIM1->PSC: %X\n", TIM1->PSC);      // Prescaler do timer
-  printf("TIM1->ARR: %X\n", TIM1->ARR);      // Valor de recarga automática (Auto-Reload Register)
-  printf("TIM1->CNT: %X\n", TIM1->CNT);      // Valor do contador do timer
-  printf("TIM1->AFR: %X\n", GPIOC->AFR[1]);
-
-
+  printf("TIM1->CR1: %X\n", TIM1->CR1);     // Configurações de controle do timer
+  printf("TIM1->SMCR: %X\n", TIM1->SMCR);   // Configurações de controle do modo do timer
+  printf("TIM1->CCMR1: %X\n", TIM1->CCMR1); // Configurações de captura/comparação do canal 1
+  printf("TIM1->CCMR2: %X\n", TIM1->CCMR2); // Configurações de captura/comparação do canal 2
+  printf("TIM1->CCER: %X\n", TIM1->CCER);   // Configurações de controle dos canais de captura/comparação
+  printf("TIM1->PSC: %X\n", TIM1->PSC);     // Prescaler do timer
+  printf("TIM1->ARR: %X\n", TIM1->ARR);     // Valor de recarga automática (Auto-Reload Register)
+  printf("TIM1->CNT: %X\n", TIM1->CNT);     // Valor do contador do timer
+  printf("TIM1->AFR: %X\n", GPIOA->AFR[1]);
 }
 
-void Encoder::init_enc_timer8()   // PC_6 PC_7 - REVISAR
+void Encoder::init_enc_timer8() // PC_6 PC_7
 {
+  // Habilitar o clock para o Timer 8 e o porto GPIOC
   RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
-  RCC->AHB1ENR |= RCC_AHB4ENR_GPIOCEN;
+  RCC->AHB4ENR |= RCC_AHB4ENR_GPIOCEN;
 
-  GPIOC->MODER &= ~GPIO_MODER_MODE6_Msk; // Tentar ~GPIO_MODER_MODE6_Msk
-  GPIOC->MODER |= GPIO_MODER_MODE6_0 | GPIO_MODER_MODE7_0;
+  // Configurar pinos GPIO para modo alternativo
+  GPIOC->MODER &= ~(GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk); // Limpar bits
+  GPIOC->MODER |= GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1;
 
-  GPIOC->OTYPER |= ~(GPIO_OTYPER_OT6 | GPIO_OTYPER_OT7);
+  GPIOC->OTYPER |= GPIO_OTYPER_OT6 | GPIO_OTYPER_OT7; // Saída push-pull
 
-  GPIOC->OSPEEDR |= GPIO_OSPEEDR_OSPEED6_0 | GPIO_OSPEEDR_OSPEED7_0;
+  GPIOC->OSPEEDR |= 0x5000; // Velocidade máxima
 
-  GPIOC->AFR[0] |= GPIO_AFRL_AFSEL6_0  | GPIO_AFRL_AFSEL7_0 ;
+  GPIOC->AFR[0] |= (3 << GPIO_AFRL_AFSEL6_Pos) | (3 << GPIO_AFRL_AFSEL7_Pos); // Selecionar função alternativa AF3 para os pinos PC6 e PC7
 
+  // Configurar Timer 8
   TIM8->CR1 = 0;
   TIM8->CR2 = 0;
   TIM8->PSC = 0;
   TIM8->ARR = 0xFFFF;
   TIM8->RCR = 0;
 
-  TIM8->CCMR1 = TIM_CCMR1_CC1S_0 | (0x0A << 4) | TIM_CCMR1_CC2S_0 | (0x0A << 12);
-  TIM8->CCER = TIM_CCER_CC1E | TIM_CCER_CC2E;
+  // Configurar modos de captura/comparação
+  TIM8->CCMR1 = TIM_CCMR1_CC1S_0 | (0x07 << 4) | TIM_CCMR1_CC2S_0 | (0x07 << 12);
+  TIM8->CCER = TIM_CCER_CC1E | TIM_CCER_CC2E; // Habilitar captura nos canais 1 e 2
   TIM8->DIER = 0;
 
-  TIM8->SMCR = 3;
-  TIM8->CR1 = TIM_CR1_CEN;
+  // Configurar modo de encoder
+  TIM8->SMCR = 3;          // Modo de encoder 3 (conta todas as transições)
+  TIM8->CR1 = TIM_CR1_CEN; // Habilitar o contador
 
-  printf("TIM8->CR1: %X\n", TIM8->CR1);      // Configurações de controle do timer
-  printf("TIM8->SMCR: %X\n", TIM8->SMCR);    // Configurações de controle do modo do timer
-  printf("TIM8->CCMR1: %X\n", TIM8->CCMR1);  // Configurações de captura/comparação do canal 1
-  printf("TIM8->CCMR2: %X\n", TIM8->CCMR2);  // Configurações de captura/comparação do canal 2
-  printf("TIM8->CCER: %X\n", TIM8->CCER);    // Configurações de controle dos canais de captura/comparação
-  printf("TIM8->PSC: %X\n", TIM8->PSC);      // Prescaler do timer
-  printf("TIM8->ARR: %X\n", TIM8->ARR);      // Valor de recarga automática (Auto-Reload Register)
-  printf("TIM8->CNT: %X\n", TIM8->CNT);      // Valor do contador do timer
+  printf("TIM8->CR1: %X\n", TIM8->CR1);     // Configurações de controle do timer
+  printf("TIM8->SMCR: %X\n", TIM8->SMCR);   // Configurações de controle do modo do timer
+  printf("TIM8->CCMR1: %X\n", TIM8->CCMR1); // Configurações de captura/comparação do canal 1
+  printf("TIM8->CCMR2: %X\n", TIM8->CCMR2); // Configurações de captura/comparação do canal 2
+  printf("TIM8->CCER: %X\n", TIM8->CCER);   // Configurações de controle dos canais de captura/comparação
+  printf("TIM8->PSC: %X\n", TIM8->PSC);     // Prescaler do timer
+  printf("TIM8->ARR: %X\n", TIM8->ARR);     // Valor de recarga automática (Auto-Reload Register)
+  printf("TIM8->CNT: %X\n", TIM8->CNT);     // Valor do contador do timer
   printf("TIM8->AFR: %X\n", GPIOC->AFR[0]);
 }
 
-void Encoder::init_enc_timer2()   // PA_5 PB_3 - AJUSTAR ORDEM DE CAPTURA
+void Encoder::init_enc_timer2() // PA_5 PB_3 - AJUSTAR ORDEM DE CAPTURA
 {
   // Ativar o clock para o Timer 2 e os GPIOs relevantes
   RCC->APB1LENR |= RCC_APB1LENR_TIM2EN;
@@ -185,8 +178,8 @@ void Encoder::init_enc_timer2()   // PA_5 PB_3 - AJUSTAR ORDEM DE CAPTURA
   GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEED3;
 
   // Configurar os pinos para AF1 (modificando os bits de AFSEL5 e AFSEL3)
-  GPIOA->AFR[0] |= GPIO_AFRL_AFSEL5_0;  // Configurar AF1 para PA_5
-  GPIOB->AFR[0] |= GPIO_AFRL_AFSEL3_0;  // Configurar AF1 para PB_3
+  GPIOA->AFR[0] |= GPIO_AFRL_AFSEL5_0; // Configurar AF1 para PA_5
+  GPIOB->AFR[0] |= GPIO_AFRL_AFSEL3_0; // Configurar AF1 para PB_3
 
   // Configurar o Timer 2
   TIM2->CR1 = 0;
@@ -203,25 +196,24 @@ void Encoder::init_enc_timer2()   // PA_5 PB_3 - AJUSTAR ORDEM DE CAPTURA
   TIM1->CCER = TIM_CCER_CC1E | TIM_CCER_CC2E; // Habilitar captura nos canais 1 e 2
 
   // Configurar o modo do encoder
-  TIM2->SMCR = 3;  // Modo de encoder usando os canais TI1 e TI2
+  TIM2->SMCR = 3; // Modo de encoder usando os canais TI1 e TI2
 
   // Habilitar o Timer 2
   TIM2->CR1 |= TIM_CR1_CEN;
 
-  printf("TIM2->CR1: %X\n", TIM2->CR1);      // Configurações de controle do timer
-  printf("TIM2->SMCR: %X\n", TIM2->SMCR);    // Configurações de controle do modo do timer
-  printf("TIM2->CCMR1: %X\n", TIM2->CCMR1);  // Configurações de captura/comparação do canal 1
-  printf("TIM2->CCMR2: %X\n", TIM2->CCMR2);  // Configurações de captura/comparação do canal 2
-  printf("TIM2->CCER: %X\n", TIM2->CCER);    // Configurações de controle dos canais de captura/comparação
-  printf("TIM2->PSC: %X\n", TIM2->PSC);      // Prescaler do timer
-  printf("TIM2->ARR: %X\n", TIM2->ARR);      // Valor de recarga automática (Auto-Reload Register)
-  printf("TIM2->CNT: %X\n", TIM2->CNT);      // Valor do contador do timer
+  printf("TIM2->CR1: %X\n", TIM2->CR1);     // Configurações de controle do timer
+  printf("TIM2->SMCR: %X\n", TIM2->SMCR);   // Configurações de controle do modo do timer
+  printf("TIM2->CCMR1: %X\n", TIM2->CCMR1); // Configurações de captura/comparação do canal 1
+  printf("TIM2->CCMR2: %X\n", TIM2->CCMR2); // Configurações de captura/comparação do canal 2
+  printf("TIM2->CCER: %X\n", TIM2->CCER);   // Configurações de controle dos canais de captura/comparação
+  printf("TIM2->PSC: %X\n", TIM2->PSC);     // Prescaler do timer
+  printf("TIM2->ARR: %X\n", TIM2->ARR);     // Valor de recarga automática (Auto-Reload Register)
+  printf("TIM2->CNT: %X\n", TIM2->CNT);     // Valor do contador do timer
   printf("TIM2->AFR: %X\n", GPIOA->AFR[0]);
   printf("TIM2->AFR: %X\n", GPIOB->AFR[0]);
+}
 
-} 
-
-void Encoder::init_enc_timer3()   // PA_6 PB_5
+void Encoder::init_enc_timer3() // PA_6 PB_5
 {
   RCC->APB2ENR |= RCC_APB1LENR_TIM3EN;
   RCC->AHB1ENR |= RCC_AHB4ENR_GPIOAEN | RCC_AHB4ENR_GPIOBEN;
@@ -229,7 +221,7 @@ void Encoder::init_enc_timer3()   // PA_6 PB_5
   GPIOA->MODER &= ~GPIO_MODER_MODE6; // Tentar ~GPIO_MODER_MODE6_Msk
   GPIOB->MODER &= ~GPIO_MODER_MODE5; // Tentar ~GPIO_MODER_MODE5_Msk
 
-  GPIOA->MODER |= GPIO_MODER_MODE6_1; 
+  GPIOA->MODER |= GPIO_MODER_MODE6_1;
   GPIOB->MODER |= GPIO_MODER_MODE5_1;
 
   GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED6;
@@ -250,4 +242,15 @@ void Encoder::init_enc_timer3()   // PA_6 PB_5
 
   TIM3->SMCR = 3;
   TIM3->CR1 = TIM_CR1_CEN;
+
+  printf("TIM3->CR1: %X\n", TIM3->CR1);     // Configurações de controle do timer
+  printf("TIM3->SMCR: %X\n", TIM3->SMCR);   // Configurações de controle do modo do timer
+  printf("TIM3->CCMR1: %X\n", TIM3->CCMR1); // Configurações de captura/comparação do canal 1
+  printf("TIM3->CCMR2: %X\n", TIM3->CCMR2); // Configurações de captura/comparação do canal 2
+  printf("TIM3->CCER: %X\n", TIM3->CCER);   // Configurações de controle dos canais de captura/comparação
+  printf("TIM3->PSC: %X\n", TIM3->PSC);     // Prescaler do timer
+  printf("TIM3->ARR: %X\n", TIM3->ARR);     // Valor de recarga automática (Auto-Reload Register)
+  printf("TIM3->CNT: %X\n", TIM3->CNT);     // Valor do contador do timer
+  printf("TIM3->AFR: %X\n", GPIOA->AFR[0]);
+  printf("TIM3->AFR: %X\n", GPIOB->AFR[0]);
 }
--- lib/Utils/ports_v2.0.h
+++ lib/Utils/ports_v2.0.h
@@ -13,82 +13,82 @@
 
 ////
 
-#define M1_PWM  PD_12 //TIM4 (CH1)
-#define M1_DIR  PB_8
-#define M1_RST  PE_15
+#define M1_PWM PD_12 // TIM4 (CH1)
+#define M1_DIR PB_8
+#define M1_RST PE_15
 #define M1_MODE PE_6
-#define M1_CST  PE_12
-#define M1_BRK  PE_10
-// #define M1_ENCA PE_9 (main) // TIM1 (CH1)  | PE_9 (H7)
-// #define M1_ENCB PF_13(main) //             | PA_9 (H7)
-#define M1_FF1  PF_14
-#define M1_FF2  PE_11
+#define M1_CST PE_12
+#define M1_BRK PE_10
+#define M1_ENCA PE_9(main)  // TIM1 (CH1)  | PE_9 (H7)
+#define M1_ENCB PF_13(main) //             | PA_9 (H7)
+#define M1_FF1 PF_14
+#define M1_FF2 PE_11
 
-#define M2_PWM  PD_13 //TIM4 (CH2)
-#define M2_DIR  PB_11
-#define M2_RST  PE_15
+#define M2_PWM PD_13 // TIM4 (CH2)
+#define M2_DIR PB_11
+#define M2_RST PE_15
 #define M2_MODE PE_6
-#define M2_CST  PE_12
-#define M2_BRK  PE_10
-// #define M2_ENCA PE_5 (main)// TIM9 (CH1) | PA_5 (H7)
-// #define M2_ENCB PE_7 (main)//            |
-#define M2_FF1  PB_7
-#define M2_FF2  PB_6
+#define M2_CST PE_12
+#define M2_BRK PE_10
+#define M2_ENCA PE_5(main) // TIM9 (CH1) | PA_5 (H7)
+#define M2_ENCB PE_7(main) //            |
+#define M2_FF1 PB_7
+#define M2_FF2 PB_6
 
-#define M3_PWM  PD_14 //TIM4 (CH3)
-#define M3_DIR  PC_0
-#define M3_RST  PE_15
+#define M3_PWM PD_14 // TIM4 (CH3)
+#define M3_DIR PC_0
+#define M3_RST PE_15
 #define M3_MODE PE_6
-#define M3_CST  PE_12
-#define M3_BRK  PE_10
+#define M3_CST PE_12
+#define M3_BRK PE_10
 // #define M3_ENCA PC_6 // TIM3 (CH1)
 // #define M3_ENCB PB_15
-#define M3_FF1  PC_2
-#define M3_FF2  PF_10
+#define M3_FF1 PC_2
+#define M3_FF2 PF_10
 
-#define M4_PWM  PD_15 //TIM4 (CH4)
-#define M4_DIR  PC_8
-#define M4_RST  PE_15
+#define M4_PWM PD_15 // TIM4 (CH4)
+#define M4_DIR PC_8
+#define M4_RST PE_15
 #define M4_MODE PE_6
-#define M4_CST  PE_12
-#define M4_BRK  PE_10
+#define M4_CST PE_12
+#define M4_BRK PE_10
 // #define M4_ENCA PF_8  // TIM13 (CH1)
 // #define M4_ENCB PD_6
-#define M4_FF1  PD_5
-#define M4_FF2  PD_4
+#define M4_FF1 PD_5
+#define M4_FF2 PD_4
 
-#define M5_PWM  PF_9 // TIM14 (CH1)
-#define M5_DIR  PD_0
-#define M5_RST  PB_13
+#define M5_PWM PF_9 // TIM14 (CH1)
+#define M5_DIR PD_0
+#define M5_RST PB_13
 #define M5_MODE PB_12
-#define M5_CST  PA_15
-#define M5_BRK  PC_9 // *
-#define M5_FF1  PB_2
-#define M5_FF2  PE_3
-#define M5_TAC  PD_7 // *
+#define M5_CST PA_15
+#define M5_BRK PC_9 // *
+#define M5_FF1 PB_2
+#define M5_FF2 PE_3
+#define M5_TAC PD_7  // *
 #define M5_DIRO PF_2 // *
 
-#define PIN_CHARGE 	PE_0
-#define PIN_CAP_LOAD 	PA_3
-#define PIN_CHIP 	PG_3
-#define PIN_FRONT 	PB_0
-#define PIN_FAULT 	PD_11
+#define PIN_CHARGE PE_0
+#define PIN_CAP_LOAD PA_3
+#define PIN_CHIP PG_3
+#define PIN_FRONT PB_0
+#define PIN_FAULT PD_11
 
-#define NRF_R_MOSI 	PC_12
-#define NRF_R_MISO 	PC_11
-#define NRF_R_SCK 	PC_10
-#define NRF_R_CSN 	PG_0
-#define NRF_R_CE 	  PB_1
-#define NRF_R_IRQ 	PH_1
-#define NRF_R_VCC 	PG_14
+#define NRF_R_MOSI PC_12
+#define NRF_R_MISO PC_11
+#define NRF_R_SCK PC_10
+#define NRF_R_CSN PG_0
+#define NRF_R_CE PB_1
+#define NRF_R_IRQ PH_1
+#define NRF_R_VCC PG_14
 
-#define NRF_S_MOSI  PC_12
-#define NRF_S_MISO  PC_11
-#define NRF_S_SCK 	PC_10
-#define NRF_S_CSN 	PF_3
-#define NRF_S_CE 	PE_13
-#define NRF_S_IRQ	PG_1
-#define NRF_S_VCC 	PA_0
+#define NRF_S_MOSI PC_12
+#define NRF_S_MISO PC_11
+#define NRF_S_SCK PC_10
+#define NRF_S_CSN PF_3
+#define NRF_S_CE PE_13
+#define NRF_S_IRQ PG_1
+#define NRF_S_VCC PA_0
 
 // #define MPU_MOSI 	PC_3 // PA_7 // TIM1
 // #define MPU_MISO 	PB_14 // PA_6 // TIM3
@@ -98,22 +98,21 @@
 
 // ******* EXTERNAL MPU 6050 ******
 // Needs to cut PF_0 PIN in main board!!!
-#define MPU_SDA 	PF_0 // I2C
-#define MPU_SCL 	PF_1 // I2C
+#define MPU_SDA PF_0 // I2C
+#define MPU_SCL PF_1 // I2C
 
-#define PIN_BUZZER 	PF_7 // TIM11 (CH1)
-#define PIN_RGB  	PD_1
-#define PIN_BATT 	PF_6
+#define PIN_BUZZER PF_7 // TIM11 (CH1)
+#define PIN_RGB PD_1
+#define PIN_BATT PF_6
 
-#define PIN_IR_LED 	PB_4
-#define PIN_IR_ADC 	PA_4
-#define PIN_TEMP 	PF_5
+#define PIN_IR_LED PB_4
+#define PIN_IR_ADC PA_4
+#define PIN_TEMP PF_5
 
+#define PIN_SELECTOR_1 PE_6
+#define PIN_SELECTOR_2 PE_2
+#define PIN_SELECTOR_3 PD_3
+#define PIN_SELECTOR_4 PE_4
 
-#define PIN_SELECTOR_1 	PE_6
-#define PIN_SELECTOR_2 	PE_2
-#define PIN_SELECTOR_3 	PD_3
-#define PIN_SELECTOR_4 	PE_4
-
-#define PIN_PB1   PF_4
-#define PIN_PB2 	PG_6
+#define PIN_PB1 PF_4
+#define PIN_PB2 PG_6
